45030bffdfa08bf71c2ca6784c7f5d12cec12fe4
ishank1212 <ishanknagpal@gmail.com>
2023-09-07 02:36:10 +0530

    updated index.js server

--- server/index.js
+++ server/index.js
@@ -12,6 +12,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Your code
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+          if(err) {
+              res.status(500).send(err)
+          }
+      });
+  })
+}
+// Your code
+
+
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
 
